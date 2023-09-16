17438a2fcb16d644b06c79a90e6981c134a3fedc
Tresor MATONDO <matondotresor5@gmail.com>
2023-09-16 10:04:07 +0100

    Fix: Configuration profile.php!

--- profile.php
+++ profile.php
@@ -97,14 +97,19 @@
                     </div>
                   </form>
                   </div>
+                  <?php if($Utilisateurs['Email']!='' || $Utilisateurs['Tel']){ ?>
                   <div class="d-flex" style="font-size:11px">
+                    <?php if($Utilisateurs['Email']!=''){ ?>
                     <a href="mailto:<?php echo $Utilisateurs['Email']; ?>" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/mail -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="3" y="5" width="18" height="14" rx="2" /><polyline points="3 7 12 13 21 7" /></svg>
                       <?php echo $Utilisateurs['Email']; ?></a>
+                      <?php } if($Utilisateurs['Tel']){ ?>
                     <a href="tel:<?php echo '243'.$Utilisateurs['Tel']; ?>" class="card-btn"><!-- Download SVG icon from http://tabler-icons.io/i/phone -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon me-2 text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
                       <?php echo '243'.$Utilisateurs['Tel']; ?></a>
+                      <?php } ?>
                   </div>
+                  <?php } ?>
                 </div>
               </div>
               <div class="col-md-6 col-lg-4">
